9752897fedaa329a7d85a83b7f60c5e80421ddce
Vikash Kumar <vikash810917@gmail.com>
2021-07-03 10:12:05 +0530

    export

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
+import Heading from "./Heading";
+import SubHeading from "./SubHeading";
+import InputQuery from "./InputQuery";
+import SubmitButton from "./SubmitButton";
+
 
 const App = () => {
   return (
     <div id="main">
-      < />
-      < />
-      < />
-      < />
+      <Heading />
+      <SubHeading />
+      <InputQuery />
+      <SubmitButton />
     </div>
   )
 }
